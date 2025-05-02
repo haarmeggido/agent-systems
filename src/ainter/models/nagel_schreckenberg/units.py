81742356fbbd66a0e0738c263f1c1bc8acaec5f3
Dominik Breksa <dominikbreksa@gmail.com>
2025-05-02 20:46:52 +0200

    Moved constants.py to units.py

---
+++ src/ainter/models/nagel_schreckenberg/units.py
@@ -0,0 +1,37 @@
+from typing import Final
+
+import numpy as np
+
+
+type PhysicalTime = np.float64              # In seconds
+type PhysicalLength = np.float64            # In meters
+type PhysicalSpeed = np.float64             # In meters per seconds
+type PhysicalAcceleration = np.float64      # In meters per second squared
+
+type DiscreteTime = np.uint32               # In time step
+type DiscreteLength = np.uint16             # In the number of grid cells
+type DiscreteSpeed = np.uint8               # In grid cells per time steps
+type DiscreteAcceleration = np.uint8        # In grid cells per time step squared
+
+DELTA_TIME: Final[PhysicalTime] = np.float64(0.5)
+
+CELL_SIZE: Final[PhysicalLength] = np.float64(0.25)
+
+SPEED_MAX: Final[PhysicalSpeed] = np.float64(20)
+SPEED_MIN: Final[PhysicalSpeed] = np.float64(-10)
+
+ACCELERATION_MAX: Final[PhysicalAcceleration] = np.float64(2.5)
+ACCELERATION_MIN: Final[PhysicalAcceleration] = np.float64(-1.5)
+
+
+def discretize_length(length: PhysicalLength) -> DiscreteLength:
+    """Converts the physical length measure into discrete Length value"""
+    return np.uint16(np.ceil(length / CELL_SIZE))
+
+def discretize_speed(speed: PhysicalSpeed) -> DiscreteSpeed:
+    """Converts the physical length measure into discrete Length value"""
+    pass
+
+def discretize_acceleration(acceleration: PhysicalAcceleration) -> DiscreteAcceleration:
+    """Converts the physical length measure into discrete Length value"""
+    pass
